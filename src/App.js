3812d8a01df85a81377af8f9ebd8047f212981cd
maran0505 <mara050720@gmail.com>
2023-09-21 11:11:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
-
 import './App.css';
 
-import React, { useState }  from 'react';
-const App = () =>   {
-  const [result, setResult] = useState("");
-
-  const handleClick = (e) => {
-    setResult(result.concat(e.target.kuna) );
-  }
-   const clear = () =>{
-    setResult("")
-   }
-return(
-  <>
-  <div className='con'>
-    
-    <form> 
-      <input type='text' value={result}></input>
-    </form>
-
-    <div className='keypad'>
-        <button OnClick={clear} id='clear'>c</button>
-        <button kuna="1" OnClick={handleClick}>1</button>
-        <button kuna="2" OnClick={handleClick}>2</button>
-        <button kuna="3" OnClick={handleClick}>3</button>
-        <button kuna="+" OnClick={handleClick}>+</button>
-        <button kuna="4" OnClick={handleClick}>4</button>
-        <button kuna="5" OnClick={handleClick}>5</button>
-        <button kuna="6" OnClick={handleClick}>6</button>
-        <button kuna="-" OnClick={handleClick}>-</button>
-        <button kuna="7" OnClick={handleClick}>7</button>
-        <button kuna="8" OnClick={handleClick}>8</button>
-        <button kuna="9" OnClick={handleClick}>9</button>
-        <button kuna="*" OnClick={handleClick}>*</button>
-        <button kuna="/" OnClick={handleClick}>/</button>
-        <button kuna="." OnClick={handleClick}>.</button>
-        <button  OnClick={handleClick}>=</button>
-    </div> 
-  </div>
-  </>
-)
-
+import React ,{useState} from "react";
+const App =()=> {
+  
+  const[count,setcount] = useState(0)
+ 
+  const handleIncrement=()=>{
+  setcount(count+1)
 }
+const handleDecrement=()=>{
+  setcount(count-1)
+}
+  return<>
+ <h1>COUNTER APP</h1>
 
+  <div className='c'>
+    Counter:{count}</div> 
+    <br></br>
+  <div className='q'>
+  <button  onClick={handleIncrement} className='c'>Increment</button>
+  <button onClick={handleDecrement}  className='d'>Decrement</button>
+  <button onClick={()=>setcount(0)} className='e'>Reset</button>
+  </div>
+
+  </>
+  };
 export default App;
